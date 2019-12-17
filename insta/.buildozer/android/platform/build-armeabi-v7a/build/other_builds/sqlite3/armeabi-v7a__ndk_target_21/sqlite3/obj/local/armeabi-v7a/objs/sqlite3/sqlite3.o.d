dc2102cff1c9acc4dd6fe898e375e02484d4fdd0
laurencengankem <laurencengankem@yahoo.fr>
2019-12-17 10:50:37 +0100

    changed insta

--- insta/.buildozer/android/platform/build-armeabi-v7a/build/other_builds/sqlite3/armeabi-v7a__ndk_target_21/sqlite3/obj/local/armeabi-v7a/objs/sqlite3/sqlite3.o.d
+++
@@ -1,2 +0,0 @@
-/home/quentin/insta/.buildozer/android/platform/build-armeabi-v7a/build/other_builds/sqlite3/armeabi-v7a__ndk_target_21/sqlite3/obj/local/armeabi-v7a/objs/sqlite3/sqlite3.o: \
-  /home/quentin/insta/.buildozer/android/platform/build-armeabi-v7a/build/other_builds/sqlite3/armeabi-v7a__ndk_target_21/sqlite3/jni/../sqlite3.c
--- insta/.buildozer/android/platform/build-armeabi-v7a/dists/myapp/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/quentin/insta/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2-python3/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o: \
-  /home/quentin/.buildozer/android/platform/android-ndk-r17c/sources/android/cpufeatures/cpu-features.c \
-  /home/quentin/.buildozer/android/platform/android-ndk-r17c/sources/android/cpufeatures/cpu-features.h
-
-/home/quentin/.buildozer/android/platform/android-ndk-r17c/sources/android/cpufeatures/cpu-features.h:
